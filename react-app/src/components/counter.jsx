2c83b060ce0daf6e04445162d67a15a98ecda4ac
Piumi Perera <piumidp96@gmail.com>
2020-11-10 10:03:31 +0530

    create counters component

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -1,9 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    render() { 
-        return <h1>Hello World</h1>;
-    }
+  state = {
+    count: 0,
+    tags: [],
+  };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  handleIncrement = (product) => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <span className="badge m-2 badge-warning">{this.state.count}</span>
+        <button
+          onClick={() => this.handleIncrement({id:1})}
+          className="btn btn-primary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
 }
- 
+
 export default Counter;
