e228b63c25d684c836fc320e8233f0a25e945bae
Piumi Perera <piumidp96@gmail.com>
2020-11-11 19:03:08 +0530

    Changed app to root component. Move state up

--- react-app/src/components/navbar.jsx
+++ react-app/src/components/navbar.jsx
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class NavBar extends Component {
-    render() { 
-        return (
-          <nav className="navbar navbar-light bg-light">
-            <a className="navbar-brand" href="#">
-              Navbar
-            </a>
-          </nav>
-        );
-    }
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Navbar
+          <span className="badge badge-pill badge-secondary m-2">
+            {this.props.totalCounters}
+          </span>
+        </a>
+      </nav>
+    );
+  }
 }
- 
+
 export default NavBar;
